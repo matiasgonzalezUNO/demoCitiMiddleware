7b4773a97342a487a4898593693713e4d69db3f0
Matias Gonzalez <matias_gonzalez@itrsa.com.ar>
2022-04-05 17:52:03 -0300

    first commit

---
+++ src/main/java/ar/com/itrsa/demoCitiMiddleware/DemoCitiMiddlewareApplication.java
@@ -0,0 +1,13 @@
+package ar.com.itrsa.demoCitiMiddleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCitiMiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCitiMiddlewareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/itrsa/demoCitiMiddleware/DemoCitiMiddlewareApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.itrsa.demoCitiMiddleware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCitiMiddlewareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
